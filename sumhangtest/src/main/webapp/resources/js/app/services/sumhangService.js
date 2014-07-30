333f456f475660a8824d0e0bf9d3cf937f37d587
youngsuno <guitaryc33@gmail.com>
2014-07-30 10:26:04 +0900

    서비스 업로드 추가

--- sumhangtest/src/main/webapp/resources/js/app/services/sumhangService.js
+++ sumhangtest/src/main/webapp/resources/js/app/services/sumhangService.js
@@ -1,27 +1,36 @@
 ﻿//This handles retrieving data and is used by controllers. 3 options (server, factory, provider) with 
 //each doing the same thing just structuring the functions/data differently.
-app.service('sumhangService', function ($http) {
-	
-    this.insertMember = function (id, name, password, nick, birth, gender, photo, pushmsg, invitemsg) {
+app.service('sumhangService', ['$http',function ($http) {
+    
+    this.addFile = function(file,uploadUrl){
+    	var fd = new FormData();
+    	fd.append('file',file);
+    	    	
     	$http({
-	        'url' : 'addMember.ajax',
-	        'method' : 'POST',
-	        'headers': {'Content-Type' : 'application/json'},
-	        'data' : {'id':id,
-	        	'name':name,
-	        	'password':password,
-	        	'nick':nick,
-	        	'birth':birth,
-	        	'gender':gender,
-	        	'photo':photo,
-	        	'push':pushmsg,
-	        	'invite':invitemsg}
-	    }).success(function (data) {
-	    	alert(data.name);
-            location.hash = '/login';
-        }).error(function (data) {
-             alert('fail');
-        });
+    	 method:'POST',
+    	 url : uploadUrl,
+    	 data : fd,
+    	 transformRequest:angular.identify,
+    	 headers:{'Content-Type':undefined}
+    	})
+    	.success(function(){
+    		alert("파일업로드 성공");
+    	})
+    	.error(function(){
+    		alert("파일업로드  실패")
+    	});
     };
-
-});
+    
+    this.addUser = function(user,userUrl){
+    	$http({
+    		method:'POST',
+    		url : userUrl,
+    		data : user,
+    		headers : {'Content-Type':'application/jason'},    		
+    	}).success(function(){
+    		alert("회원가입 성공")
+    	}).error(function(){
+    		alert("회원가입 실패")
+    	})
+    }
+}]);
