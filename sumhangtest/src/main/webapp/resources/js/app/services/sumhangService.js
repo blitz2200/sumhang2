679ebc5029ab8260db194a590ada5366a7cfc21a
cyj0730 <cyj0730@nate.com>
2014-07-31 18:40:54 +0900

    여행 파일 업로드

--- sumhangtest/src/main/webapp/resources/js/app/services/sumhangService.js
+++ sumhangtest/src/main/webapp/resources/js/app/services/sumhangService.js
@@ -2,6 +2,7 @@
 //each doing the same thing just structuring the functions/data differently.
 app.service('sumhangService', ['$http',function ($http) {
     
+	/*유저파일 업로드 시작*/
     this.addFile = function(file,uploadUrl){
     	var fd = new FormData();
     	fd.append('file',file);    	    	
@@ -31,5 +32,38 @@ app.service('sumhangService', ['$http',function ($http) {
     	}).error(function(){
     		alert("회원가입 실패")
     	})
-    }
-}]);
+    };	//유저 파일 업로드 끝
+    
+    
+    /*여행 파일 업로드*/
+    this.addTripFile = function(tripfile,uploadUrl){
+    	var fd1 = new FormData();
+    	fd1.append('tripfile',tripfile);    	    	
+    	$http({
+    	 method:'POST',
+    	 url : uploadUrl,
+    	 data : fd1,
+    	 transformRequest:angular.identify,
+    	 headers:{'Content-Type':undefined}
+    	})
+    	.success(function(){
+    		alert("파일업로드 성공");
+    	})
+    	.error(function(){
+    		alert("파일업로드  실패")
+    	});
+    };		
+    
+    this.addTrip = function(trip,tripUrl){
+    	$http({
+    		method:'POST',
+    		url : tripUrl,
+    		data : trip,
+    		headers : {'Content-Type':'application/jason'},    		
+    	}).success(function(){
+    		alert("여행 파일업로드 성공")
+    	}).error(function(){
+    		alert("여행 파일업로드 실패")
+    	})
+    };
+}]);	//여행 파일 업로드 끝
