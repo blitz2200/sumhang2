218da023ee96820005c6d79dac0adfd219d96808
youngsuno <guitaryc33@gmail.com>
2014-07-31 11:41:00 +0900

    메인 컨트롤러 추가

--- sumhangtest/src/main/webapp/resources/js/app/controllers/controllers.js
+++ sumhangtest/src/main/webapp/resources/js/app/controllers/controllers.js
@@ -11,19 +11,17 @@
 app.controller('LoginController', ['$scope', 'sumhangFactory', 
                                      function ($scope, sumhangFactory) {
     
-    
-    function loginRequest(){
-    	sumhangFactory.loginRequest()
-    	.success(function(){
-    		
-    	}).error(function (error){
-    		
-    	});
-    }
 
     $scope.loginRequest = function () {        
-        location.hash = '/main';
+        alert('로그인 시작')
+        location.hash='/main';     
     };
+    
+    
+    function loginRequest(){
+
+    }
+
 }]);
 
 app.controller('JoinMemberController', ['$scope', 'sumhangService', function ($scope, sumhangService) {
@@ -91,15 +89,27 @@ app.controller('AddTripController', ['$scope', 'addTripFactory',
     
 }]);
 
-app.controller('MainController', function ($scope, sumhangService) {
+app.controller('MainController',['$scope','mainFactory', function ($scope, mainFactory) {
 
-    init();
+	//메인컨트롤러 실행시 메인 함수가 실행한다. 메인함수가 하는역활 디비에서 메인 화면에 뿌려줄 자료 가져와서 
+	//메인 html파일과 연결 시킴 
+    main();
 
-    function init() {
+    function main() {
+    	alert('메인 컨트롤러 시작');
     	
+    	mainFactory.listMain()
+    	.success(function(data){
+    		alert('로그인 성공 넘어온 데이타는 ?:'+ data);
+    		//메인 객체와 디비에서 넘어온 객체 연결 
+    		$scope.trips=data;
+    		alert("메인에 넘길데이타 :" +$scope.trips);		
+    	}).error(function (error){
+    		alert('로그인 실패');
+    	});
     }
     //you need to describe event handler below... 
-});
+}]);
 
 app.controller('ModifyMemberController', function ($scope, sumhangService) {
 
