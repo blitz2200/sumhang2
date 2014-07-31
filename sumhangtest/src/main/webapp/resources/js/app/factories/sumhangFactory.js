f885a47b2594e0d639ef79e0414305db7eff9f4b
youngsuno <guitaryc33@gmail.com>
2014-07-31 12:11:58 +0900

    메인 리스트 주석 추가

--- sumhangtest/src/main/webapp/resources/js/app/factories/sumhangFactory.js
+++ sumhangtest/src/main/webapp/resources/js/app/factories/sumhangFactory.js
@@ -59,23 +59,27 @@ angular.module('sumhangApp')
 	return addTripFactory;
 }]);
 
+
+//메인 리스트 팩토리 
 angular.module('sumhangApp').factory('mainFactory',['$http', function($http){
 	
 	var mainFactory = {};
 	
 	mainFactory.listMain = function(){
-	
+		//메인 리스트 ajax로 요청
 		return $http({
+			//main.ajax로 서버에서  전송 스프링 컨트롤러에서 @Requestmapping main찾아서 실행
 			'url' :"main.ajax",
 			'method' : 'GET'    		
 		}).success(function(data){
+			//db에서 자료 가져오기 성공하면 찍어보기 
 			alert('메인 팩토리 성공'+data);
 			
 		}).error(function(){
 			alert('메인 자료받아오기 실패');
 		})
 	};
-	
+	//컨트롤러에 작업 완료후 객체 넘겨주기 
 	return mainFactory;
 
 }])
