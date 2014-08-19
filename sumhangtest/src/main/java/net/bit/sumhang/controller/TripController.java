2059360b6945059b71b9a57b6c8744ead2123018
youngsuno <guitaryc33@gmail.com>
2014-08-19 17:32:43 +0900

    여행게시판  갤러리 추가

--- sumhangtest/src/main/java/net/bit/sumhang/controller/TripController.java
+++ sumhangtest/src/main/java/net/bit/sumhang/controller/TripController.java
@@ -2,18 +2,15 @@ package net.bit.sumhang.controller;
 
 
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpSession;
 
+import net.bit.sumhang.domain.TimeLineVO;
 import net.bit.sumhang.domain.TripVO;
 import net.bit.sumhang.domain.UserStatusVO;
 import net.bit.sumhang.domain.UserVO;
@@ -24,7 +21,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -90,61 +86,11 @@ public class TripController {
 			//timeLine DB에 insert
 			//sqlSession.insert("tripControlMapper.addTimeLine", timeLineVO );
 			
-
-			
-			
-			
 			
 			
 		return trip;
 	}	
 	
-	//여행 등록 파일 저장
-	@RequestMapping(value = "/addTripFile", method = RequestMethod.POST)
-	public @ResponseBody String addTripFile(HttpServletRequest req, @RequestPart MultipartFile tripfile){
-		
-		System.out.println("넘어온 파일 데이타는?"+tripfile);
-			
-		//파일 유효성 체크	
-		if(!tripfile.isEmpty()){
-				try{
-					//바이트에 넘어온 파일 저장
-					byte[] bytes=tripfile.getBytes();
-					
-					//업로드 파일 이름 변수에 저장
-					String tripPhotoFile=tripfile.getOriginalFilename();
-					System.out.println("업로드 파일 이름:"+ tripPhotoFile);
-					
-					//저장할 파일 폴더 스트링에 저장 (서블릿 콘텍스트 홈으로 설정)
-					String rootPath = new HttpServletRequestWrapper(req).getRealPath("/");
-					System.out.println("서블렛 콘텍스트 홈 :"+rootPath);//서블렛 콘텍스트홈
-					
-					//파일 저장 풀경로 만들기 
-					File dir = new File(rootPath + File.separator +"resources"+File.separator
-					+"images"+File.separator+"tripPhotoFile");
-					//디렉토리가 없다면 디렉토리 생성
-					if(!dir.exists())
-						dir.mkdir();
-					
-					System.out.println("사진 업로드 절대경로 :" +dir.getAbsolutePath());
-					
-					//서버에 파일 저장
-					File serverFile = new File(dir.getAbsolutePath() + File.separator + tripPhotoFile);
-					BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
-					
-					stream.write(bytes);
-					stream.close();
-					
-					System.out.println("여행게시판사진성공="+tripPhotoFile);
-				}catch (Exception e){
-					System.out.println("업로드 실패 = >" + e.getMessage());
-				}
-			}else{
-					System.out.println("파일 업로드 실패! 파일이 없습니다.");
-			}
-					
-		return "";
-	}
 	
 	//메인 리스트 게시판 시작
 	@SuppressWarnings({ "rawtypes", "unchecked" })
