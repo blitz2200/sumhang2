1fde3570f74055a60f42bb3532ae10c278f1f20e
youngsuno <guitaryc33@gmail.com>
2014-08-19 16:10:42 +0900

    조인멤버 카메라 갤러리 추가

--- sumhangtest/src/main/java/net/bit/sumhang/controller/UserController.java
+++ sumhangtest/src/main/java/net/bit/sumhang/controller/UserController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpSession;
+import javax.xml.ws.RequestWrapper;
 
 import net.bit.sumhang.auth.UserAuth;
 import net.bit.sumhang.auth.UserAuthService;
@@ -161,4 +162,48 @@ public class UserController {
 	
 		return "";
 	}	
+	
+		@RequestMapping(value="/userPhoto", method = RequestMethod.POST)
+		public @ResponseBody String addUserPhoto(@RequestPart MultipartFile userPhoto){
+			System.out.println("카메라 사진 촬영 파일"+userPhoto);
+
+			if(!userPhoto.isEmpty()){ //파일 유효성 체크
+				try{
+					// 바이트에 넘어온 파일 저장
+					byte[] bytes=userPhoto.getBytes(); 
+					
+					//업로드 파일이름 변수 에저장
+					String userPhotoFile=userPhoto.getOriginalFilename()+".jpg";
+					System.out.println("업로드 파일이름 : "+userPhotoFile);
+					
+					//저장할 파일 폴더 스트링에 저장 (서블렛 컨텍스트 홈으로 설정)
+					/*String rootPath = new HttpServletRequestWrapper(req).getRealPath("/");*/
+					String rootPath = "c:/userUploads";
+					System.out.println("유저포터 저장위치: "+rootPath);
+					
+					//파일 저장 풀경로 만들기 
+			/*		File dir = new File(rootPath +File.separator +"resources"+File.separator
+							+"images"+ File.separator + "userPhotoFiles");*/
+					File dir = new File(rootPath);
+					//디렉토리가 없다면 디렉토리 생성
+					if(!dir.exists())
+						dir.mkdir();
+					
+					System.out.println("사진 업로드 절대경로 :" +dir.getAbsolutePath());
+					
+					//서버에 파일 저장 
+					File serverFile = new File(dir.getAbsolutePath() + File.separator + userPhotoFile);
+					BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
+					stream.write(bytes);
+					stream.close();
+					
+					System.out.println("파일 업로드 성공="+userPhotoFile);
+				}catch (Exception e){
+					System.out.println("파일 업로드 실패  = >" + e.getMessage());
+				}
+			}else{
+					System.out.println("파일 업로드실패 파일이 없습니다.");
+			}
+			return "";
+		}
 }
