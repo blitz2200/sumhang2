c4340a15a3d6365011acf1cb4940546326697d5c
youngsuno <guitaryc33@gmail.com>
2014-08-20 09:49:30 +0900

    app 오류 수정

--- sumhangtest/src/main/webapp/resources/js/app/app.js
+++ sumhangtest/src/main/webapp/resources/js/app/app.js
@@ -8,7 +8,7 @@
 	loadCSS("");
 });
 
-var app = angular.module('sumhangApp', ['ngRoute','sumhangCordova','pushNotify','pageslide-directive', 'ngResource', 'ui.bootstrap']);
+var app = angular.module('sumhangApp', ['ngRoute','sumhangCordova','pageslide-directive', 'ngResource', 'ui.bootstrap']);
 
 //This configures the routes and associates each route with a view and a controller
 app.config(function ($routeProvider) {
