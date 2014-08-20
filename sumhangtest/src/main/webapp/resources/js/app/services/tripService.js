e4aa9a01f671f29fece7680f8b8e3c6ed2997d21
youngsuno <guitaryc33@gmail.com>
2014-08-20 21:41:38 +0900

    폰갭과 충돌 수정

--- sumhangtest/src/main/webapp/resources/js/app/services/tripService.js
+++ sumhangtest/src/main/webapp/resources/js/app/services/tripService.js
@@ -1,23 +1,5 @@
 app.service('tripService', ['$http',function ($http) {
     
-	/*여행 파일 업로드*/
-    this.addTripFile = function(sa,tripfile){
-    	var fd1 = new FormData();
-    	fd1.append('tripfile',tripfile);    	    	
-    	$http({
-    	 method:'POST',
-    	 url : sa+"addTripFile.ajax",
-    	 data : fd1,
-    	 transformRequest:angular.identify,
-    	 headers:{'Content-Type':undefined}
-    	})
-    	.success(function(){
-    		alert("파일업로드 성공");
-    	})
-    	.error(function(){
-    		alert("파일업로드  실패")
-    	});
-    }
     
     this.addTrip = function(sa,trip){
     	$http({
