e9d515a8f4bdbf6f55f966fc6b8fb8e35e4e7d60
cyj0730 <cyj0730@nate.com>
2014-08-21 20:12:42 +0900

    타임라인 댓글

--- sumhangtest/src/main/java/net/bit/sumhang/controller/TimeLineController.java
+++ sumhangtest/src/main/java/net/bit/sumhang/controller/TimeLineController.java
@@ -1,10 +1,13 @@
 package net.bit.sumhang.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
+
+import net.bit.sumhang.domain.UserVO;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
 
 @Controller
 public class TimeLineController {
@@ -32,5 +37,29 @@ public class TimeLineController {
 			list = sqlSession.selectList("tripControlMapper.getTimelineList",travelNo);
 			
 			return list;
+		}
+		
+		@SuppressWarnings("rawtypes")
+		@RequestMapping(value="/timelineReply", method = RequestMethod.POST)
+		public @ResponseBody String updateTimeline(HttpSession session, @RequestBody String timelineVar){
+			
+			
+			System.out.println("타임라인 Reply 시작... ");
+			System.out.println("넘어온 travelNo"+timelineVar);
+					
+			UserVO userVO=(UserVO)session.getAttribute("user");
+			
+			Gson gson = new Gson();
+				
+			Map<String, String> map=new HashMap<String, String>();
+			map.put("userNo", Integer.toString(userVO.getUserNo()));
+			
+			map=gson.fromJson(timelineVar, Map.class);
+	
+			
+			/*List <Map> list = new ArrayList<Map>();	*/
+	/*		list = sqlSession.selectList("tripControlMapper.getTimelineList",travelNo);*/
+			
+			return "replySuccess";
 		}
 }
