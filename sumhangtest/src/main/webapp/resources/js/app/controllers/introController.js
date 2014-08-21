fc7efa996330605ca5749be9134de0d040b58d1a
youngsuno <guitaryc33@gmail.com>
2014-08-21 16:26:14 +0900

    슬라이드

--- sumhangtest/src/main/webapp/resources/js/app/controllers/introController.js
+++ sumhangtest/src/main/webapp/resources/js/app/controllers/introController.js
@@ -4,19 +4,30 @@ app.controller('IntroController', ['$scope','userFactory','globalFactory',
 	CarouselDemoCtrl($scope)
 	
 	function CarouselDemoCtrl($scope) {
-		  $scope.myInterval = 5000;
-		  var slides = $scope.slides = [];
+		  $scope.myInterval = 1000;
+		  
+		  $scope.slides=[
+		     {image:'http://192.168.0.88:8080/resources/images/tripPhotoFile/1818.jpg',
+		    	 test:['하하하']},
+		    	 
+		    	 {image:'http://192.168.0.88:8080/resources/images/tripPhotoFile/1820.jpg'},
+		    		 {image:'http://192.168.0.88:8080/resources/images/tripPhotoFile/1831.jpg'},
+		  ]
+		  /*var slides = $scope.slides = [];
 		  $scope.addSlide = function() {
-		    var newWidth = 400 + slides.length;
+		    var newWidth = 400 
 		    slides.push({
-		      image: '/resoures/images/' + newWidth + '/300',
+		      image: 'http://192.168.0.88:8080/resources/images/tripPhotoFile/' + newWidth + '/300',
+		     // http://192.168.0.88:8080/resources/images/tripPhotoFile/
 		      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
 		        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
 		    });
 		  };
 		  for (var i=0; i<4; i++) {
 		    $scope.addSlide();
-		  }
+		  }*/
+		  
+		  var slides=$scope.slides;
 	}
 		
 	
