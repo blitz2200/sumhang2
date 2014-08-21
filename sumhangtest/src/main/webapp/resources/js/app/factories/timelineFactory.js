6a1fa0388986f27e8e0315983bdd7a6374b0eefe
cyj0730 <cyj0730@nate.com>
2014-08-21 20:19:39 +0900

    타임라인 댓글

--- sumhangtest/src/main/webapp/resources/js/app/factories/timelineFactory.js
+++ sumhangtest/src/main/webapp/resources/js/app/factories/timelineFactory.js
@@ -19,6 +19,74 @@ app.factory('timeLineFactory',['$http', function($http){
 		})
 	};
 	//컨트롤러에 작업 완료후 객체 넘겨주기 
+	
+var suser;
+	
+	//리플 출력 시작
+	timeLineFactory.timelineListReply=function(sa,travelNo,timelineNo){
+		console.log('timelineFactory 시작 넘어온 travelNo확인 : ' + travelNo);
+		console.log('timelineFactory 시작 넘어온 timelineNo확인 : ' + timelineNo);
+		return $http({
+			'url' :sa+"timelineListReply.ajax",
+			'method': 'POST',
+			'data' : {'travelNo':travelNo,
+						'timelinelNo':timelineNo}
+		}).success(function(data){
+			console.log('여행 세부 게시판 리플 가져오기 성공'+JSON.stringify(data));
+		
+			console.log('data[length-1].suser_no:   '+data[data.length-1].suser_no);
+			
+			/*세션에든 유저 넘버*/
+			 suser=data[data.length-1].suser_no;
+			 console.log('세션에든 유저 넘버'+suser);
+			 
+			/*세션 유저 넘버 자르기*/ 
+			data.splice(data.length-1);
+			console.log('마지막 userno 짜른후 data:'+ JSON.stringify(data));
+			
+			/*세션 유저 넘버와 리플 게시판 유저넘버 비교후 true false 붙여 넣기*/
+			for(var i=0; i<data.length;i++){
+				if(data[i].user_no==suser){
+					console.log('트루 user_no[  ' +i+']  :'+data[i].user_no);
+					data[i].del='true';//제이슨 추가
+				}
+				else{
+					console.log('실패 user_no[  ' +i+']  :'+data[i].user_no);
+					data[i].del='false';
+				}
+			}
+			
+			console.log('최종 연산후 data: '+JSON.stringify(data));
+			
+			
+			
+		}).error(function(){
+			console.log('여행 세부 게시판 리플 가져오기 실패');
+		})
+	};
+
+	
+	//리플 입력 시작
+	
+	timeLineFactory.timelineReply=function(sa,timelineReply,travelNo,timelineNo){
+		console.log('팩토리에 넘어온 메인상세 리플: '+timelineReply);
+		console.log('팩토리에 넘어온 메인상세 방번호: '+travelNo);
+		console.log('팩토리에 넘어온 타임라인 번호: '+timelineNo);
+		return $http({
+			'url' :sa+"timelineReply.ajax",
+			'method' : 'POST',
+			'data' : {'travelNo':travelNo,
+						'timelineNo':timelineNo,
+					  'timelineReply':timelineReply 	
+					 }
+		
+		}).success(function(data){
+			console.log('여행 상세 게시판 리플 입력 성공??   '+data);
+		}).error(function(){
+			console.log('여행 상세 게시판 리플 입력 실패');
+		})
+	};
+	
 	return timeLineFactory;
 
 }])
