959b3dd3d9f69dea99553ae2a3a1d2f29d10515a
youngsuno <guitaryc33@gmail.com>
2014-08-21 12:26:11 +0900

    유저사진들 작은사진으로 교체

--- sumhangtest/src/main/webapp/resources/js/app/services/userService.js
+++ sumhangtest/src/main/webapp/resources/js/app/services/userService.js
@@ -1,23 +1,6 @@
 app.service('userService', [ '$http', function($http) {
 
-	/*유저파일 업로드 시작*/
-	this.addFile = function(sa, file) {
-		var fd = new FormData();
-		fd.append('file', file);
-		$http({
-			method : 'POST',
-			url : sa + "addFile.ajax",
-			data : fd,
-			transformRequest : angular.identify,
-			headers : {
-				'Content-Type' : undefined
-			}
-		}).success(function() {
-			alert("파일업로드 성공");
-		}).error(function() {
-			alert("파일업로드  실패")
-		});
-	}
+
 
 	//유저추가
 	this.addUser = function(sa, user) {
