fb4755449ce524f9a59a9dc9e0feab3ee6bcafd4
youngsuno <guitaryc33@gmail.com>
2014-08-21 12:26:38 +0900

    작은사진으로 교체

--- sumhangtest/src/main/java/net/bit/sumhang/controller/TripUploadController.java
+++ sumhangtest/src/main/java/net/bit/sumhang/controller/TripUploadController.java
@@ -1,8 +1,13 @@
 package net.bit.sumhang.controller;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.mortennobel.imagescaling.ResampleOp;
+import com.mortennobel.imagescaling.AdvancedResizeOp;
+
 @Controller
 public class TripUploadController {
 	
@@ -20,26 +28,22 @@ public class TripUploadController {
 	private SqlSession sqlSession;
 	
 	@RequestMapping(value = "/tripGalleryPhoto", method = RequestMethod.POST)
-		public @ResponseBody String addTripFile( @RequestPart MultipartFile tripGalleryPhoto){
+		public @ResponseBody String addTripFile(HttpServletRequest req, @RequestPart MultipartFile tripGalleryPhoto){
 		System.out.println("넘어온 파일 데이타는?"+tripGalleryPhoto);
 			
 		//파일 유효성 체크	
 		if(!tripGalleryPhoto.isEmpty()){
-				try{
+				try{				
 					//바이트에 넘어온 파일 저장
 					byte[] bytes=tripGalleryPhoto.getBytes();
-					
+					//InputStream is = tripGalleryPhoto.getInputStream();
 					//업로드 파일 이름 변수에 저장
 					String tripPhotoFile=tripGalleryPhoto.getOriginalFilename()+".jpg";
 					System.out.println("업로드 파일 이름:"+ tripPhotoFile);
 					
-					//저장할 파일 폴더 스트링에 저장 (서블릿 콘텍스트 홈으로 설정)
-					/*String rootPath = new HttpServletRequestWrapper(req).getRealPath("/");
-					System.out.println("서블렛 콘텍스트 홈 :"+rootPath);//서블렛 콘텍스트홈*/
-					String rootPath = "/tripUploads";
-					//파일 저장 풀경로 만들기 
-					/*File dir = new File(rootPath + File.separator +"resources"+File.separator
-					+"images"+File.separator+"tripPhotoFile");*/
+					
+					String rootPath = req.getSession().getServletContext().getRealPath("resources/images/tripPhotoFile");
+					
 					File dir = new File(rootPath);
 					//디렉토리가 없다면 디렉토리 생성
 					if(!dir.exists())
@@ -50,11 +54,40 @@ public class TripUploadController {
 					//서버에 파일 저장
 					File serverFile = new File(dir.getAbsolutePath() + File.separator + tripPhotoFile);
 					BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
-					
 					stream.write(bytes);
 					stream.close();
 					
 					System.out.println("여행게시판사진성공="+tripPhotoFile);
+					//리사이즈할 원본 파일 폴더경로
+					
+					//리사이즈 할 파일 파일확장자까지;
+					String sumnailePath=req.getSession().getServletContext().getRealPath("resources/images/tripPhotoFile");
+					
+					File sumnailFile =new File(sumnailePath+ File.separator + tripPhotoFile);
+					System.out.println("불러올 섬네일 파일 경로"+sumnailFile);
+					
+					
+					BufferedImage resizeFile = ImageIO.read(sumnailFile); 
+					ResampleOp resampleOp = new ResampleOp(450, 300);
+					resampleOp.setUnsharpenMask(AdvancedResizeOp.UnsharpenMask.VerySharp);
+					BufferedImage rescaled = resampleOp.filter(resizeFile, null);
+					
+					
+					//리사이즈할 파일 폴더 경로
+					String sumNailImage = "s_"+tripPhotoFile;
+					System.out.println("저장할 최후 파일 이름 : "+ sumNailImage);
+					File sumnailDir = new File(sumnailePath);
+					System.out.println("저장할 폴더:  "+sumnailDir);
+					
+					//없다면 폴더 생성					
+					if(!sumnailDir.exists())
+						sumnailDir.mkdir();
+					
+					File sumNailImageFile = new File(sumnailDir.getAbsolutePath()+File.separator+sumNailImage);
+					
+					ImageIO.write(rescaled, "JPG",sumNailImageFile);
+					System.out.println("섬네일 파일 업로드 성공");
+					
 				}catch (Exception e){
 					System.out.println("업로드 실패 = >" + e.getMessage());
 				}
