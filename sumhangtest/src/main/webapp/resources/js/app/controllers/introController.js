d473c381b5944af67c327872af9764fd2e4c8029
KimYongNyun <ecarusblue@nate.com>
2014-08-21 15:48:32 +0900

    slider

--- sumhangtest/src/main/webapp/resources/js/app/controllers/introController.js
+++ sumhangtest/src/main/webapp/resources/js/app/controllers/introController.js
@@ -1,6 +1,28 @@
 app.controller('IntroController', ['$scope','userFactory','globalFactory', 
                                    function ($scope, userFactory,globalFactory) {
 	
+	CarouselDemoCtrl($scope)
+	
+	function CarouselDemoCtrl($scope) {
+		  $scope.myInterval = 5000;
+		  var slides = $scope.slides = [];
+		  $scope.addSlide = function() {
+		    var newWidth = 400 + slides.length;
+		    slides.push({
+		      image: '/resoures/images/' + newWidth + '/300',
+		      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+		        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+		    });
+		  };
+		  for (var i=0; i<4; i++) {
+		    $scope.addSlide();
+		  }
+	}
+		
+	
+	
+	
+	
 	function loginCheck(){
 		userFactory.loginCheck(globalFactory.serverAddress)
 		.success(function(data){ 
