d88470a490de00c93d7d30f5d1bf16ab025ae7f6
youngsuno <guitaryc33@gmail.com>
2014-08-22 09:47:50 +0900

    초대 푸시 업데이트

--- sumhangtest/src/main/webapp/resources/js/app/controllers/inviteController.js
+++ sumhangtest/src/main/webapp/resources/js/app/controllers/inviteController.js
@@ -1,42 +1,39 @@
-app.controller('inviteController',['$scope','mainFactory','globalFactory',
-                                 function ($scope, mainFactory,globalFactory) {
-	alert('시작1');
-	$scope.serverAddress=globalFactory.serverAddress;
-	
+app.controller('InviteController',['$scope','mainFactory','globalFactory','pushInviteFactory',
+                                 function ($scope, mainFactory,globalFactory,pushInviteFactory) {
+	$scope.serverAddress=globalFactory.serverAddress;	
 	var sa=globalFactory.serverAddress;
-	$scope.serverAddress=sa;
-	
-	 
-     
-	alert('시작');
+
 	invite();
+	
 	function invite(){
-		alert('왔음');
+		alert('인바이트 시작')
 		mainFactory.invite(globalFactory.serverAddress)
 		.success(function(data){
 			console.log('invite 성공 넘어온 데이터는?:'+JSON.stringify(data));
-			
-			$scope.inviteTrips=data;
-			
+			$scope.inviteTrips=data;			
 			console.log("invite 메인에 넘길데이타 :" +JSON.stringify($scope.inviteTrips));
-		})
-		
-	}
-	
-	//초대 푸쉬 날리기
- 	$scope.inviteUser=function(){
-		pushInviteTrip()
-	};
-	
-	function pushInviteTrip(){
-		console.log('여행초대하기 푸시버튼 클릭')
-		mainFactory.pushInviteTrip(sa,userNo)
-		.sucess(function(){
-			console.log('여행초대하기 푸시성공');
 		}).error(function(error){
-			console.log('여행참가하기 푸시실패');
+			console.log('error');
 		})
-		
+				
 	}
+	
+	
+	//여행 초대하기 푸쉬 날리기 버튼 클릭이후 함수
+	$scope.inviteUser=function(inviteUser){
+		
+			console.log('여행 초대하기 버튼 클릭');
+			alert('여행 초대할 사람 번호'+inviteUser);
+			
+			pushInviteFactory.pushInviteUser(sa,inviteUser)
+			.sucess(function(){
+				console.log('여행초대하기 푸시성공');
+			}).error(function(error){
+				console.log('여행참가하기 푸시실패');
+			})
+			
+		}
+	
+
 	 
 }]);
