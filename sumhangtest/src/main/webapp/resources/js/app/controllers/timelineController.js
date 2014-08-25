0dbd661fd06acdcb03f568ee1dcad9dfc5ce64a9
youngsuno <guitaryc33@gmail.com>
2014-08-25 18:04:58 +0900

    타임라인글쓰기

--- sumhangtest/src/main/webapp/resources/js/app/controllers/timelineController.js
+++ sumhangtest/src/main/webapp/resources/js/app/controllers/timelineController.js
@@ -1,5 +1,11 @@
-app.controller('TimeLineController', ['$scope', '$route','$routeParams','tripDetailFactory','timeLineFactory','modalService','globalFactory',
-                                      function ($scope, $route, $routeParams, tripDetailFactory, timeLineFactory, modalService, globalFactory) {
+app.controller('TimeLineController', ['$scope', '$route','$routeParams',
+                                      'tripDetailFactory','timeLineFactory',
+                                      'globalFactory','Camera',
+                                      'timeLineUploadService','timeLineAddFactory',
+                                      function ($scope, $route, $routeParams, 
+                                    		  tripDetailFactory, timeLineFactory,
+                                    		  globalFactory,Camera,
+                                    		  timeLineUploadService,timeLineAddFactory) {
 
 
 	
@@ -9,8 +15,6 @@ app.controller('TimeLineController', ['$scope', '$route','$routeParams','tripDet
 	
 	console.log('timeLine 시작');
 	var travelNo = $routeParams.travelNo;	
-	var timelineNo=$routeParams.timelineNo;
-	
 	//$scope.timeLine = $route.current.locals.timeLine; //resolve에 있는 변수를 scope에 넘겨준다.
 	getTravelInfo(travelNo)
 	getTimelineList(travelNo);
@@ -33,72 +37,118 @@ app.controller('TimeLineController', ['$scope', '$route','$routeParams','tripDet
     	});
 	}
 	
-	/*댓글 쓰기 modal 연결*/
-	$scope.goWriteReply=function(){	   
-		
-		/*상세보기 삭제 modal 설정*/
-		 var modalDefaults = {
-				 	backdrop: true,
-		            keyboard: true,
-		            modalFade: true,
-	                templateUrl: '/partials/timelineReplyModal.html',
-	            };
-		 
-		 var modalOptions = {
-		            closeButtonText: '취소',
-		            actionButtonText: '입력',
-		            headerText: '게시물 삭제',
-		            bodyText: '게시물을 정말  삭제하시겠습니까?'
-		        };
-		 
-		 /*modal 페이지*/
-		 modalService.showModal(modalDefaults, modalOptions).then(function () {
-			 console.log('아무거나')
-			 $scope.updateTimelineReply(travelNo,timelineNo);  
+	$scope.timeLineReplyList = true;
+	$scope.writeTimeLine=false;
+	
+	
+	
+	
+	/*타임라인글쓰기 카메라*/
+			//디비 서버에 넘길 변수 (갤러리나 카메라 둘중하나)
+	var timeLinePhotoFile=null;
+			//유저사진 하드에 저장할 변수 (갤러리나 카메라 둘중하나) 
+	var timeLinePhotoMultipartFile;
+	
+	$scope.timeLineCapturePhoto=function(){
+		timeLineCamera();
+	}
+	
+	  function timeLineCamera(){
+		  Camera.getPicture(function(cameraImage) {
+			  var documentCameraImage=document.getElementById("documentTimeLine");
+			      documentCameraImage.src=cameraImage;
+	            $scope.$apply(function() {
+	                alert('카메라 사진 경로:'+cameraImage);
+	                userPhotoFile = cameraImage.substr(cameraImage.lastIndexOf('/') + 1)+".jpg";
+	                userPhotoMultipartFile=cameraImage;
+	                alert('디비에 넣을 사진 이름 '+timeLinePhotoFile);
+	                alert('서버에 저장할 파일 경로'+timeLinePhotoMultipartFile);
+	            });
+	        }, function(error) {
+	            $scope.$apply(function() {
+	                $scope.error = error;
+	            });
+	        }, {
+	            destinationType: Camera.DestinationType.FILE_URL,
+	            sourceType: Camera.PictureSourceType.CAMERA,
+	            encodingType: Camera.EncodingType.JPEG,
+	            quality: 50
 	        });
-	}
-	
-	//리플 리스트 시작
-	function timelineListReply(travelNo,timelineNo){
-		console.log('timelineReply 시작')
-		timeLineFactory.timelinelListReply(sa,travelNo,timelineNo)
-		.success(function(data){
-			console.log('디비에서 꺼내온 main detail reply data:'+JSON.stringify(data));
-		
-			$scope.replys=data;
-			console.log('html에 출력할 리플라이 데이타 '+JSON.stringify($scope.replys));
-		}).error(function(error){
-			console.log('timelinelReply 콘트롤러 실패');
-		})
-	}
-	
-	
-	
-	//타임라인 댓글 달기 시작
-	
-	$scope.updateTimelineReply=function(timelineNo){
-		timelineReply(timelineNo);
-	}
-	
-	
-	
-	function timelineReply(timelineNo){
-		console.log('timelineReply 입력 함수 시작');
-		var timelineData=$scope.timelinelReply;
-			
-		console.log('받아온	timelineData  값은: '+timelineData);
-		console.log('받아온 travelNo 값은: '+travelNo);
-		console.log('받아온 timelineNo 값은: '+timelineNo);
-		
-		timeLineFactory.timelineReply(sa,timelineData,travelNo,timelineNo)
-		.success(function(timelineNo){
-			console.log('타임라인 리플 입력완료')
-		}).error(function(error){
-			console.log('타임라인 리플 입력 실패')
-		})
-	}
-	
+	  }
+	  
+	  
+	//갤러리에서 사진 가져오기
+	  $scope.timeLineGalleryPhoto=function(){
+		  timeLineGallery();
+	  }
+	  function timeLineGallery(){
+		  Camera.getPicture(function(galleryImage) {	
+			  var documentGalleryImage=document.getElementById("documentTimeLine");
+		      	  documentGalleryImage.src=galleryImage;
+				if (galleryImage.substring(0,21)=="content://com.android") {
+          		  photo_split=galleryImage.split("%3A");
+          		  galleryImage="content://media/external/images/media/"+photo_split[1];
+				}
+	            $scope.$apply(function() {
+	                alert('갤러리 사진 경로:'+galleryImage);
+	                timeLinePhotoFile=galleryImage.substr(galleryImage.lastIndexOf('/') + 1)+".jpg";
+	                timeLinePhotoMultipartFile=galleryImage;
+	                alert('디비에 넣을 사진 이름 '+timeLinePhotoFile);
+	                alert('서버에 저장할 파일 경로'+timeLinePhotoMultipartFile);
+	               	                
+	            });
+	        }, function(error) {
+	            $scope.$apply(function() {
+	                $scope.error = error;
+	            });
+	        }, {
+	            destinationType: Camera.DestinationType.FILE_URI,
+	            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+	            encodingType: Camera.EncodingType.JPEG,
+	            quality: 50
+	        });
+	  }
+	  
+	  /*타임라인 글쓰기 비지니스 로직*/	  
 
+		/*타임라인 글쓰기*/
+		$scope.goAddTimeLine=function(){
+			//타임라인 리스트 감추기
+			$scope.timeLineReplyList = false;
+			$scope.writeTimeLine=true;			
+		}
+		
+		$scope.addTimeLineRequest=function(){
+			$scope.submitted = true;
+			if($scope.writeTimeLineForm.timelineContextText.$valid){
+			
+				alert('타임라인 글쓰기 시작')
+				var timeLine=$scope.timeLine;
+				alert('타임라인 객체 내용:' + JSON.stringify(timeLine));
+				if (timeLinePhotoFile != null) {				
+					//회원가입 객체에 파일이름 추가 
+					timeLine.timeLinePhoto=timeLinePhotoFile;
+					timeLine.timeLineSphoto='s_'+timeLinePhotoFile;
+					
+					alert("사진 파일 추가후 업로드"+JSON.stringify(timeLine));				
+					//파일객체 서비스에 전송
+					timeLineUploadService.timeLineuploadPhoto(sa,timeLineMultipartFile);	
+				}else{
+					timeLine.timeLinePhoto="defaultTimeLinePhoto.png";
+					timeLine.timeLineSphoto='s_defaultTimeLinePhoto.png';				
+				}				
+					timeLineAddFactory.timeLineAdd(sa,timeLine,travelNo)
+					.success(function(){
+						$scope.timeLineReplyList = true;
+						$scope.writeTimeLine=false;
+					}).error(function(error){
+						alert('타임라인 입력 콘트롤러 실패');
+					})
+			}else{
+				alert('내용을 입력하세요');
+			}
+		}
+		
 	
 	/*로그아웃*/
 	$scope.logout=function(){		
@@ -130,8 +180,8 @@ app.controller('TimeLineController', ['$scope', '$route','$routeParams','tripDet
 	
 	
 	/*타임라인쓰기 라우터*/
-	$scope.addTimeLine=function(){
-		$scope.location.path('/addTimeline');		
+	$scope.goAddTrip=function(){
+		$scope.location.path('/addTrip');		
 	}	
 	
 	/*메인 라우터*/
