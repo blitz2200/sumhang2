d6d20555e1c06bebca293dadb14744b41997b350
youngsuno <guitaryc33@gmail.com>
2014-08-25 18:03:42 +0900

    타임라인글쓰기

---
+++ sumhangtest/src/main/webapp/resources/js/app/factories/timeLineAddFactory.js
@@ -0,0 +1,18 @@
+app.factory('timeLineAddFactory',['$http', function($http){
+	var timeLineAddFactory = {};
+	
+	 timeLineAddFactory.timeLineAdd=function(sa,timeLine,travelNo){
+		timeLine.tboardNo=travelNo;
+		return $http({
+			'url': sa+"timeLineAdd.ajax",
+			'method' : 'POST',
+			'data' : timeLine
+		}).success(function(){
+			alert('타임라인 글쓰기 팩토리 성공')
+		}).error(function(){
+			alert('타임랑니 글쓰기 팩토리 실패')
+		})
+	};
+	
+	return timeLineAddFactory;
+}])
