1f0a34100715dbb964c70449883ff039ec922bb0
youngsuno <guitaryc33@gmail.com>
2014-08-25 18:03:12 +0900

    타입라인 글쓰기

---
+++ sumhangtest/src/main/webapp/resources/js/app/services/timeLineUploadService.js
@@ -0,0 +1,32 @@
+app.service('timeLineUploadService', ['$http',function ($http) {
+    
+    
+    this.timeLineuploadPhoto = function(sa,timeLineMultipartFile){
+    	alert('메인상세페이지 편집 여행사진 업로드 서비스 시작');
+    	
+		  var win = function (r) {
+			    console.log("Code = " + r.responseCode);
+			    console.log("Response = " + r.response);
+			    console.log("Sent = " + r.bytesSent);
+		  }
+	
+		 var fail = function (error) {
+			    alert("An error has occurred: Code = " + error.code);
+			    console.log("upload error source " + error.source);
+			    console.log("upload error target " + error.target);
+		 }
+	
+	 	var options = new FileUploadOptions();
+		  
+		  options.fileKey = "timeLinePhoto";
+		  options.fileName = timeLineMultipartFile
+		  					 substr(timeLineMultipartFile.lastIndexOf('/') + 1);
+		  	
+		  alert('최종 업로드할  수정 여행 파일:'+options.fileName);
+		  
+		  var ft = new FileTransfer();
+		  ft.upload(timeLineMultipartFile, encodeURI(sa+"timeLineUpload.ajax"), win, fail, options);		
+	
+    }
+  
+}]);
