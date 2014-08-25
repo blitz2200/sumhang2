4c53fb8c6fb05e1a295416c45eb3e927fddc7468
youngsuno <guitaryc33@gmail.com>
2014-08-25 18:12:33 +0900

    초대 수정111111

--- sumhangtest/src/main/webapp/resources/js/app/controllers/inviteController.js
+++ sumhangtest/src/main/webapp/resources/js/app/controllers/inviteController.js
@@ -1,5 +1,7 @@
-app.controller('InviteController',['$scope','mainFactory','globalFactory','pushInviteFactory',
-                                 function ($scope, mainFactory,globalFactory,pushInviteFactory) {
+app.controller('InviteController',['$scope','mainFactory','globalFactory',
+                                    'modalService','pushInviteFactory',
+                                 function ($scope, mainFactory,globalFactory,
+                                		 modalService,pushInviteFactory) {
 	$scope.serverAddress=globalFactory.serverAddress;	
 	var sa=globalFactory.serverAddress;
 
@@ -27,10 +29,38 @@ app.controller('InviteController',['$scope','mainFactory','globalFactory','pushI
 				
 	}
 	
+	//초대할 유저새로고침
+	$scope.inviteUserRefresh=function(){		
+		invite();
+	}
+	
 	
 	//여행 초대하기 푸쉬 날리기 버튼 클릭이후 함수
-	$scope.inviteUser=function(inviteUser){
+	$scope.goInviteUser=function(inviteUser,inviteUserNick){
+		/*alert(inviteUserNick);
+			alert(JSON.stringify(inviteUserNick));*/
+		var enterTripModalDefaults = {
+		          backdrop: true,
+		                keyboard: true,
+		                modalFade: true,
+		                  templateUrl: 'partials/tripDetailDeleteModal.html',
+		              };
+		     
+		     var enterTripModalOptions = {
+		                closeButtonText: '취소',
+		                actionButtonText: '보내기',
+		                headerText: '여행 초대하기',		                
+		                bodyText:inviteUserNick+'님에게 여행 초대하기를 보내시겠습니까?'
+		            };
+		     
+		     modalService.showModal(enterTripModalDefaults, enterTripModalOptions).then(function () {
+		    	 inviteTripUser(inviteUser);
+		          });
 		
+	
+	};
+	
+	function inviteTripUser(inviteUser){	
 			console.log('여행 초대하기 버튼 클릭');
 			console.log('여행 초대할 사람 번호'+inviteUser);			
 			pushInviteFactory.pushInviteUser(sa,inviteUser)
