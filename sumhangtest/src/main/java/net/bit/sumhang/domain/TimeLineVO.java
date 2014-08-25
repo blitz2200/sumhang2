61fcea450277a4a06b82d78ad7243c1ebccab323
youngsuno <guitaryc33@gmail.com>
2014-08-25 18:05:26 +0900

    타임라인글쓰기

--- sumhangtest/src/main/java/net/bit/sumhang/domain/TimeLineVO.java
+++ sumhangtest/src/main/java/net/bit/sumhang/domain/TimeLineVO.java
@@ -5,106 +5,62 @@ public class TimeLineVO {
 	private int tboardNo;
 	private int userNo;
 	private String timeLinePhoto;
+	private String timeLineSphoto;
 	private String timeLineDesc;
 	private int timeLineLike;
-	
-	
 	
 	
 	public int getTimeLineNo() {
 		return timeLineNo;
 	}
-
-
-
-
 	public void setTimeLineNo(int timeLineNo) {
 		this.timeLineNo = timeLineNo;
 	}
-
-
-
-
 	public int getTboardNo() {
 		return tboardNo;
 	}
-
-
-
-
 	public void setTboardNo(int tboardNo) {
 		this.tboardNo = tboardNo;
 	}
-
-
-
-
 	public int getUserNo() {
 		return userNo;
 	}
-
-
-
-
 	public void setUserNo(int userNo) {
 		this.userNo = userNo;
 	}
-
-
-
-
 	public String getTimeLinePhoto() {
 		return timeLinePhoto;
 	}
-
-
-
-
 	public void setTimeLinePhoto(String timeLinePhoto) {
 		this.timeLinePhoto = timeLinePhoto;
 	}
-
-
-
-
+	public String getTimeLineSphoto() {
+		return timeLineSphoto;
+	}
+	public void setTimeLineSphoto(String timeLineSphoto) {
+		this.timeLineSphoto = timeLineSphoto;
+	}
 	public String getTimeLineDesc() {
 		return timeLineDesc;
 	}
-
-
-
-
 	public void setTimeLineDesc(String timeLineDesc) {
 		this.timeLineDesc = timeLineDesc;
 	}
-
-
-
-
 	public int getTimeLineLike() {
 		return timeLineLike;
 	}
-
-
-
-
 	public void setTimeLineLike(int timeLineLike) {
 		this.timeLineLike = timeLineLike;
 	}
-
-
-
-
+	
 	@Override
 	public String toString() {
-		return "TimeLineVO [timeLineNo=" + timeLineNo + ", tboardNo=" + tboardNo
-				+ ", userNo=" + userNo + ", timeLinePhoto="
-				+ timeLinePhoto + ", timeLineDesc=" + timeLineDesc + ", timeLineLike="
+		return "TimeLineVO [timeLineNo=" + timeLineNo + ", tboardNo="
+				+ tboardNo + ", userNo=" + userNo + ", timeLinePhoto="
+				+ timeLinePhoto + ", timeLineSphoto=" + timeLineSphoto
+				+ ", timeLineDesc=" + timeLineDesc + ", timeLineLike="
 				+ timeLineLike + "]";
 	}
-
 	
-	
-
 
 }
