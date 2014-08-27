c42316712f30b51cc1217a8660c039cecee2d6b3
blitz2200 <blitz2200@naver.com>
2014-08-27 09:42:19 +0900

    spinner

--- sumhangtest/src/main/webapp/resources/js/app/controllers/loginController.js
+++ sumhangtest/src/main/webapp/resources/js/app/controllers/loginController.js
@@ -1,23 +1,20 @@
-app.controller('LoginController', ['$scope','userFactory','globalFactory', 
-                                     function ($scope, userFactory,globalFactory) {
+app.controller('LoginController', ['$scope','userFactory','usSpinnerService','globalFactory', 
+                                     function ($scope, userFactory, usSpinnerService, globalFactory) {
     
     
     function loginRequest(loginInfo){
     	
-    	
+    	usSpinnerService.spin('spinner-1');
     	userFactory.loginRequest(globalFactory.serverAddress,loginInfo)
-    	.success(function(data){
-    		
-    		console.log(data);
-			if(data == ""){
-				console.log('아이디와 비밀번호를 확인해 주세요');
+    	.then(function(response){
+    		usSpinnerService.stop('spinner-1');
+    		console.log(response);
+			if(response == ""){
+				alert('아이디와 비밀번호를 확인해 주세요');
 				$scope.location.path('/login');
 			}else{
 				$scope.location.path('/main');
-			}
-    		
-    	}).error(function (error){
-    		
+			}    		
     	});
     	    	
     }
